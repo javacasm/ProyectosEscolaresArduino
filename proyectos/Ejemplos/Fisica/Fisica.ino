a337639b2d646d88e9074777d01506e27197c2a6
javacasm <javacasm@gmail.com>
2016-03-01 14:34:23 +0100

    codigo arduino Bitbloq

--- proyectos/Ejemplos/Fisica/Fisica.ino
+++ proyectos/Ejemplos/Fisica/Fisica.ino
@@ -7,14 +7,80 @@
 
 
 /***   Global variables and function definition  ***/
-int zumbador_0 = 6;LiquidCrystal lcd_0(0);int led_Fin_9 = 9;int led_inicio_3 = 3;int sensor_fin_10 = 10;int sensor_inicio_2 = 2;int boton_0 = 12;bqSoftwareSerial puerto_serie_0(0,1,9600);void CheckReset (){if(digitalRead(12) == 1){digitalWrite(led_Fin_9,LOW);digitalWrite(led_inicio_3,LOW);/*
-Borramos la segunda línea
-*/lcd_0.setCursor(0,0);lcd_0.print("Cinematica!!!");lcd_0.setCursor(0,1);lcd_0.print("                                                    ");}}
+int zumbador_0 = 6;
+LiquidCrystal lcd_0(0);
+int led_Fin_9 = 9;
+int led_inicio_3 = 3;
+int sensor_fin_10 = 10;
+int sensor_inicio_2 = 2;
+int boton_0 = 12;
 
-/***   Setup  ***/
-void setup(){lcd_0.begin(16, 2);lcd_0.clear();pinMode(led_Fin_9, OUTPUT);pinMode(led_inicio_3, OUTPUT);pinMode(sensor_fin_10, INPUT);pinMode(sensor_inicio_2, INPUT);pinMode(boton_0, INPUT);lcd_0.setBacklight(HIGH);lcd_0.setCursor(0,0);lcd_0.print("Cinematica!!!");lcd_0.setCursor(0,1);lcd_0.print("                                                    ");}
+bqSoftwareSerial puerto_serie_0(0,1,9600);
+
+// Comprobamos si se ha pulsado el botón
+void CheckReset ()
+{if(digitalRead(12) == 1)
+{
+  // Apagamos los dos leds
+  digitalWrite(led_Fin_9,LOW);
+  digitalWrite(led_inicio_3,LOW);
+  /* Borramos la segunda línea*/
+  lcd_0.setCursor(0,0);
+  lcd_0.print("Cinematica!!!");
+  lcd_0.setCursor(0,1);
+  lcd_0.print("                                                    "); // Borramos la 2ª línea
+  }
+}
+
+/***   Configuración ***/
+void setup()
+{
+// Inicializamos la pantalla
+lcd_0.begin(16, 2);
+lcd_0.clear();
+// Establecemos los pines de entrada y salida
+pinMode(led_Fin_9, OUTPUT);
+pinMode(led_inicio_3, OUTPUT);
+pinMode(sensor_fin_10, INPUT);
+pinMode(sensor_inicio_2, INPUT);
+pinMode(boton_0, INPUT);
+// Dibujamos la pantalla inicial
+lcd_0.setBacklight(HIGH);
+lcd_0.setCursor(0,0);
+lcd_0.print("Cinematica!!!");
+lcd_0.setCursor(0,1);
+lcd_0.print("                                                    ");
+}
 
 /***   Loop  ***/
-void loop(){if(digitalRead(2) == 1){int tiempo_Inicial = millis();puerto_serie_0.println(tiempo_Inicial);tone(zumbador_0,261,20);
-delay(20);digitalWrite(led_inicio_3,HIGH);digitalWrite(led_Fin_9,LOW);delay(100);while (digitalRead(10) == 0){delay(100);if(digitalRead(10) == 1){int tiempo_Final = millis();tone(zumbador_0,440,20);
-delay(20);float Tiempo_transcurrido = (tiempo_Final - tiempo_Inicial);digitalWrite(led_Fin_9,HIGH);lcd_0.setCursor(0,1);lcd_0.print(Tiempo_transcurrido);puerto_serie_0.println(Tiempo_transcurrido);break;}CheckReset();}}CheckReset();}
+void loop()
+{
+  if(digitalRead(sensor_inicio_2) == 1)
+  {
+    int tiempo_Inicial = millis(); // Empezamos a medir tiempos
+    puerto_serie_0.println(tiempo_Inicial);
+    tone(zumbador_0,261,20); // Tono inicial
+    delay(20);
+    digitalWrite(led_inicio_3,HIGH);
+    digitalWrite(led_Fin_9,LOW);
+    delay(100);
+    while (digitalRead(sensor_fin_10) == 0) // Esperamos mientras el sensor de fin no está activo
+    {
+      delay(100);
+      if(digitalRead(sensor_fin_10) == 1) // Si se ha activado el sensor de fin medimos el tiempo
+      {
+        int tiempo_Final = millis();  // Medimos el tiempo final
+        tone(zumbador_0,440,20); // Tono de fin
+        delay(20);
+        float Tiempo_transcurrido = (tiempo_Final - tiempo_Inicial);
+        digitalWrite(led_Fin_9,HIGH);
+        lcd_0.setCursor(0,1);
+        lcd_0.print(Tiempo_transcurrido);
+        puerto_serie_0.println(Tiempo_transcurrido);
+        break;
+      }
+      CheckReset(); // Comprobamos el pulsador
+    }
+  }
+  CheckReset(); // Comprobamos el pulsador
+}
